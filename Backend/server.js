0d43c53723cd4e1057638aa06b172c6e9d5ca8c7
Samarth MAlhotra <samarthmalhotra4200@gmail.com>
2025-10-19 16:24:30 +0530

    Add feature

--- Backend/server.js
+++ Backend/server.js
@@ -6,31 +6,32 @@ import chatRoutes from "./routes/chat.js";
 import authRouter from "./routes/authentication.js";
 import passport from "passport";
 import "./authSetup.js";
-import { useCallback } from "react";
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const app = express();
 //
 app.use(express.json());
-const origins = [
-  "http://localhost:8080",
-  "https://grow-aibackend.onrender.com",
-];
+// const allowedOrigins = [
+//   "http://localhost:8080",
+//   "https://grow-aibackend.onrender.com",
+// ];
 //
 app.use(express.urlencoded({ extended: true }));
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
 const port = process.env.PORT;
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (origins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by cors"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+
+// app.use(cors(corsOptions));
+// This ensures preflight requests are handled
+app.use("/api", cors());
 app.use(passport.initialize());
 app.use("/api", chatRoutes);
 //console.log("Hello");
--- Frountend/src/environment.js
+++ Frountend/src/environment.js
@@ -1,4 +1,4 @@
-const is_Prod = true;
+const is_Prod = false;
 const server = is_Prod
   ? "https://grow-aibackend.onrender.com"
   : "http://localhost:8080";
